5fc9366f436ab2fa2689f9526e4950833d31679e
g18m0999 <36505493+g18m0999@users.noreply.github.com>
2020-07-18 11:24:02 +0200

    Update Library.fs

--- Patterns/Library.fs
+++ Patterns/Library.fs
@@ -2,6 +2,7 @@
 module Patterns
 #endif
 #if !INTERACTIVE
+#if !INTERACTIVE
 module Patterns
 open System
 
@@ -14,27 +15,26 @@ type Pattern =
 | BlackP
 | WhiteP 
 | UnknownP
-|OneOrMore of Pattern
-|Exactly of int * Pattern
-|FewerThan of int * Pattern
-|Either of Pattern
-|Anything
-|EndOfCells
-|Sequence of Cell List
-|ZeroOrMore of Pattern
+| OneOrMore of Pattern
+| Exactly of int * Pattern
+| FewerThan of int * Pattern
+| Either of Pattern
+| Anything
+| EndOfCells
+| Sequence of Cell List
+| ZeroOrMore of Pattern
 
 let _charToCell ch =
   match ch with 
-  | 'b'| 'B' -> BlackP
-  | 'w'| 'W'-> WhiteP
-  | _ -> UnknownP
+  | 'b'| 'B' -> Black
+  | 'w'| 'W'-> White
+  | _ -> Yellow
 
 let _cellToChar cell =
   match cell with 
-  | Black -> Some 'b' 
-  | White -> Some 'w'
-  | Yellow -> Some '.'
-  | _ -> None 
+  | Black ->  "b"
+  | White ->  "w"
+  | Yellow ->  "."
 
 let toCells (str:string) =
   let rec helper index cellList = 
@@ -46,40 +46,56 @@ let toCells (str:string) =
   helper 0 []
 
 let fromCells (cells: Cell List) = 
-  List.map _cellToChar cells
+    let a = List.rev (List.map _cellToChar cells)
+    List.fold (fun state elem-> state+elem) "" a
 
-let cwnt n p d =
-    match (List.filter(p) d).Length < n with 
-    |true -> Some //
-    | _ -> None   
 
 let p2Cell (ptn: Pattern) =
     match ptn with
     | BlackP -> Black
     | WhiteP -> White
     | UnknownP -> Yellow
-    | _ -> failwith "Not implemented"
+    | _ -> failwith "Not implemented"   
 
-
-let exactlyMatch num ptn cells =
+let matchFn num p cells =
     let out = List.take num cells
-    let predicate = (fun entry -> entry<> (p2Cell ptn))  //returns everything not as speficies
-    let m = List.filter predicate out
+    let fn = (fun a -> a<> (p2Cell p))  //returns everything not as speficies
+    let m = List.filter fn out
     match (List.length m) > 0 with
     | true -> None
     | _ -> Some out
 
-let patternMatch (p: Pattern) (d: Cell list): Option<Cell list>=
-    let rec helper (p: Pattern) (d: Cell list) (r: Option<Cell list>) =
-         match p with
-         |BlackP -> r@[Some Black] // Some 'b'
-         |WhiteP -> r@[Some White] // some 'w'
-         |UnknownP -> r@[Some Yellow] // some '.'
+let patternMatch1 p d = 
+    match p with 
          |ZeroOrMore a -> helper a d r
          |OneOrMore a -> helper a d r
-         |Exactly (n, p) -> exactlyMatch n p d  
-         |FewerThan (n, p) -> cwnt n p d 
-    helper p d r
+         |Exactly (n, p) -> matchFn n p d  
+         |FewerThan (n, p) -> failwith "Not implemented"//cwnt (n-1) p d 
+         |Sequence lst -> failwith "Not implemented"
+         |Either a -> failwith "Not implemented"
+         |Anything -> failwith "Not implemented"
+         |EndOfCells -> failwith "Not implemented"
+         | _ -> failwith "Not implemented"
+
+let patternMatch (p: Pattern) (d: Cell list): Option<Cell list>=
+    let rec helper (p: Pattern) (d: Cell list) (r: Cell list) =
+         match p with
+         |BlackP -> Some ( r@[Black] ) // Some 'b'
+         |WhiteP -> Some ( r@[White] ) // some 'w'
+         |UnknownP -> Some ( r@[Yellow] ) // some '.'
+
+    helper p d []
+(*
+
+let cwnt n p d =
+    match (List.filter(p) d).Length < n with 
+    |true -> patternMatch p d 
+    | _ -> None  
+
+
+
+*)
+ 
 
  // toCells "bbw_B!W"
 
@@ -87,12 +103,6 @@ let patternMatch (p: Pattern) (d: Cell list): Option<Cell list>=
 //                                                                                                                                                                                        |> fromCells
 
 #endif
-
-let toCells v = failwith "Not implemented"
-
-let fromCells v = failwith "Not implemented" 
-
-let patternMatch pattern cells = failwith "Not implemented"
 
 let find pattern cells = failwith "Not implemented"
 
