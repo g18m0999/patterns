60579e42172347321cfd24aa402d79ee00864245
g18m0999 <36505493+g18m0999@users.noreply.github.com>
2020-07-21 16:33:04 +0200

    Oceans: Mission complete

--- Patterns/Library.fs
+++ Patterns/Library.fs
@@ -3,107 +3,187 @@ module Patterns
 #endif
 #if !INTERACTIVE
 #if !INTERACTIVE
-module Patterns
-open System
+module Patterns 
 
 type Cell =
 | Black
 | White 
-| Yellow
+| Unknown
 
 type Pattern = 
-| BlackP
-| WhiteP 
-| UnknownP
-| OneOrMore of Pattern
-| Exactly of int * Pattern
-| FewerThan of int * Pattern
-| Either of Pattern
-| Anything
-| EndOfCells
-| Sequence of Cell List
-| ZeroOrMore of Pattern
+| BlackP | WhiteP | UnknownP
+|OneOrMore of Pattern
+|Exactly of int * Pattern | FewerThan of int * Pattern
+|Either of Pattern * Pattern
+|Anything | EndOfCells
+|Sequence of Pattern List
+|ZeroOrMore of Pattern
 
-let _charToCell ch =
-  match ch with 
+type Func =
+  |MakeUnknown |MakeWhite |MakeBlack 
+  |Delete 
+  |Double
+
+
+let charToCell c =
+  match c with 
   | 'b'| 'B' -> Black
   | 'w'| 'W'-> White
-  | _ -> Yellow
+  | _ -> Unknown
 
-let _cellToChar cell =
+let cellToChar cell =
   match cell with 
-  | Black ->  "b"
-  | White ->  "w"
-  | Yellow ->  "."
+  | Black -> "b"
+  | White -> "w"
+  | Unknown -> "." 
 
 let toCells (str:string) =
   let rec helper index cellList = 
     match index < str.Length with
-    | false -> cellList
     | true -> 
-      let cell = _charToCell str.[index]
-      helper (index+1) cellList@[cell]
-  helper 0 []
+            let cell = charToCell str.[index]
+            helper (index+1) (cell::cellList)
+    | false -> cellList
+  List.rev (helper 0 [])
 
 let fromCells (cells: Cell List) = 
-    let a = List.rev (List.map _cellToChar cells)
-    List.fold (fun state elem-> state+elem) "" a
+  let x = List.map(cellToChar) cells
+  List.fold (fun state e -> state + e) "" x
 
-
-let p2Cell (ptn: Pattern) =
+let ToCell (ptn: Pattern) =
     match ptn with
     | BlackP -> Black
     | WhiteP -> White
-    | UnknownP -> Yellow
-    | _ -> failwith "Not implemented"   
+    | UnknownP -> Unknown
+    | _ -> failwith "Pattern not recognized"   
 
-let matchFn num p cells =
-    let out = List.take num cells
-    let fn = (fun a -> a<> (p2Cell p))  //returns everything not as speficies
-    let m = List.filter fn out
-    match (List.length m) > 0 with
-    | true -> None
-    | _ -> Some out
 
-let patternMatch1 p d = 
-    match p with 
-         |ZeroOrMore a -> helper a d r
-         |OneOrMore a -> helper a d r
-         |Exactly (n, p) -> matchFn n p d  
-         |FewerThan (n, p) -> failwith "Not implemented"//cwnt (n-1) p d 
-         |Sequence lst -> failwith "Not implemented"
-         |Either a -> failwith "Not implemented"
-         |Anything -> failwith "Not implemented"
-         |EndOfCells -> failwith "Not implemented"
-         | _ -> failwith "Not implemented"
 
-let patternMatch (p: Pattern) (d: Cell list): Option<Cell list>=
-    let rec helper (p: Pattern) (d: Cell list) (r: Cell list) =
-         match p with
-         |BlackP -> Some ( r@[Black] ) // Some 'b'
-         |WhiteP -> Some ( r@[White] ) // some 'w'
-         |UnknownP -> Some ( r@[Yellow] ) // some '.'
+let matchCell (ptn: Pattern) (lst: Cell list) =
+    //matches a black/white/unknown cells with given pattern 
+    match lst.Length > 0 with 
+    |true ->
+        let x = lst.Head            
+        let y = ToCell ptn
+        match x = y with
+            |true -> Some [x]
+            |false -> None 
+    |false -> None 
 
-    helper p d []
+let rec zeroOrMore (ptn: Pattern) (lst: Cell list) (rtn:Cell List) = 
+//matches zero or more patterns  
+    match lst.Length > 0 with
+    |true -> 
+      let x = lst.Head 
+      let y = ToCell ptn 
+      match x = y with 
+      | false -> Some rtn
+      | true -> zeroOrMore ptn (lst.Tail) (rtn@[x])
+    |false -> Some rtn
+   
+let rec oneOrMore (ptn: Pattern) (lst: Cell list) (rtn:Cell List) = 
+//matches one or more patterns
+    match lst.Length > 0 with
+    |true -> 
+      let x = lst.Head 
+      let y = ToCell ptn
+      match rtn.Length > 0 with
+      |true ->
+         match x = y with
+         | true -> zeroOrMore ptn (lst.Tail) (rtn@[x])
+         | false -> Some rtn
+      |false ->
+          match x = y with 
+           | false -> None 
+           | true -> zeroOrMore ptn (lst.Tail) (rtn@[x])
+    |false -> None 
+
+let exact (n:int) (ptn: Pattern) (lst: Cell list) (rtn:Cell List) =
+ //matches exactly n instances of a pattern
+    let out = zeroOrMore ptn lst rtn
+    match out with
+    | None -> Some []
+    | Some ans ->
+        match ans.Length = n with
+        |true -> Some (List.take n ans)
+        |false -> None
+
+let fewerThan (n:int) (ptn: Pattern) (lst: Cell list) (rtn:Cell List) =
+//matches fewer than n instances of a pattern
+    let out = zeroOrMore ptn lst rtn
+    match n > 0 with
+     |true ->
+            match out with
+            | None -> Some []
+            | Some ans ->
+                match ans.Length > n with
+                |true -> Some (List.take n ans)
+                |false -> None
+     |false -> None 
+
+let seqMatch (plst: Pattern list) (lst: Cell list) (output: Cell List) =
+//matches a sequence of patterns
+    let cList = List.map (fun ptn -> ToCell ptn) plst
+    let boolList = List.mapi (fun index entry -> entry=lst.[index]) cList 
+    let result= List.fold (fun state flagEntry -> state&&flagEntry) true boolList
+    match result with
+    | true -> Some cList
+    | false -> None
+
+let orMatch (ptn1: Pattern) (ptn2: Pattern) (lst: Cell List) =
+//matches either of two patterns
+    let x = matchCell ptn1 lst
+    let y = matchCell ptn2 lst
+    match x , y with
+    | Some p, Some q -> Some p
+    | None, None -> None
+    | Some ans, None | None, Some ans -> Some ans
+
+let anyMatch (lst: Cell list) =
+//matches any cell
+    let x = lst.Head 
+    Some (x::[]) 
+
+let patternMatch (ptn: Pattern) (lst: Cell list): Option<Cell list>=
+    match ptn with
+        |BlackP -> matchCell ptn lst 
+        |WhiteP -> matchCell ptn lst
+        |UnknownP -> matchCell ptn lst
+        |ZeroOrMore a -> zeroOrMore a lst []
+        |OneOrMore a -> oneOrMore a lst []
+        |Exactly (n, p) ->  exact n p lst [] 
+        |FewerThan (n, p) -> fewerThan n p lst []
+        |Sequence ptns -> seqMatch ptns lst []
+        |Either (a,m) -> orMatch a m lst
+        |Anything  -> Some (lst.Head::[]) 
+        |EndOfCells -> match lst with |[] -> Some [] | _ -> None  
+        | _ -> failwith "Not implemented"
 (*
+let Find (ptn: Pattern) (lst: Cell list): Option<Cell list> = 
+    let x = patternMatch ptn lst
+    let y = List.findIndex(patternMatch ptn lst ) x
+    match (x, y) with 
+    | :? (Cell list*int) -> Some (x, y)   
+    |_ -> None 
+    *)
 
-let cwnt n p d =
-    match (List.filter(p) d).Length < n with 
-    |true -> patternMatch p d 
-    | _ -> None  
+let Map (func:Func) (ptn:Pattern) (lst:Cell list) =
+     match func with 
+     |MakeUnknown -> lst.Head = Unknown >> lst
+     |MakeWhite -> lst.Head = White >> lst
+     |MakeBlack -> lst.Head = Black >> lst
+     |Delete -> List.filter(fun x -> not((ToCell ptn) = x )lst
+     |Double -> let x = (ToCell ptn) >> x::lst
+     | _ -> failwith "Not implemented"
 
 
-
-*)
- 
-
- // toCells "bbw_B!W"
-
-// toCells "bbw_B!W"
-//                                                                                                                                                                                        |> fromCells
-
-#endif
-
-let find pattern cells = failwith "Not implemented"
+    
+(*
+let Find (ptn: Pattern) (lst: Cell list): Option<Cell list> = 
+    let x = patternMatch p d
+    let y = List.find() 
+     (*| -> some (x, y)
+     | -> none*) 
 
 let map func pattern cells = failwith "Not implemented"
+*)
