907d480f2c5a7e109413eca54752e4fe156e1347
g18m0999 <36505493+g18m0999@users.noreply.github.com>
2020-07-24 14:52:01 +0200

    Add files via upload

---
+++ File1.fs
@@ -0,0 +1,186 @@
+﻿
+module Patterns
+open System
+
+type Cell = 
+| Positive
+| Negative
+| Unknown
+
+type Pattern = 
+| BlackP
+| WhiteP
+| UnknownP
+| Anything
+| EndOfCells
+| ZeroOrMore of Pattern
+| OneOrMore of Pattern
+| Exactly of int*Pattern
+| FewerThan of int*Pattern
+| Sequence of Pattern list
+| Either of Pattern*Pattern
+
+let _charToCell (ch:char) =
+  match (Char.ToLower ch) with
+  |'b' -> Positive
+  |'w' -> Negative
+  |_ -> Unknown
+
+let _cellToString (cell: Cell) =
+  match cell with
+  | Positive -> "b"
+  | Negative -> "w"
+  | Unknown -> "."
+
+let _isPatternMatchingToCell (ptn: Pattern) (cell:Cell) =
+  match ptn with
+  | BlackP -> Positive = cell
+  | WhiteP -> Negative = cell
+  | UnknownP -> Unknown = cell
+  | Anything -> true
+  | _ -> failwith "Pattern does not represent any cell case"
+
+let toCells (str:string)  = 
+  let rec helper (index:int) (out:Cell list) = 
+    match index<(String.length str) with 
+    | false -> out
+    | true -> 
+      let cell = _charToCell str.[index]
+      helper (index+1) (cell::out)
+
+  let unorderedList = helper 0 []
+  List.rev unorderedList
+
+let fromCells (cells: Cell list) =
+  let seq = List.map _cellToString cells
+  String.concat String.Empty seq
+
+let patternMatch (ptn:Pattern) (_cells:Cell list) : Option<Cell List> =
+  let check (ptn:Pattern) (cells:Cell list) =
+    match cells with 
+    | [] -> None
+    | h::_ -> 
+      match _isPatternMatchingToCell ptn h with
+      | true -> Some h
+      | _ -> None
+
+  let rec helper (seq:Pattern) (cellList:Cell list) (result:Cell list) = 
+    match seq with 
+    | BlackP | WhiteP | UnknownP  -> 
+      let option = check seq cellList
+      match option with
+      | Some v -> Some (result@[v], cellList.Tail)
+      | _ -> None 
+
+    | ZeroOrMore a -> 
+      match lst.Length > 0 with
+      | false -> Some (rtn, cellList)
+      | true ->
+        let ans = adder ptn cellList rtn
+        match ans with
+        | None -> Some (rtn, cellList)
+        | Some (ptnCells, rest) -> helper ptn rest ptnCells
+          
+
+    | OneOrMore ptn ->
+      match cellList.Length > 0 with
+      | true ->
+        let nested = helper (ZeroOrMore ptn) cellList result
+        match nested with
+        | Some (ptnCells, remCells) -> 
+            match (ptnCells.Length > 0 ) with
+            | false -> None
+            | true -> Some (ptnCells, remCells)
+        | _ -> None
+      | false -> None
+
+    | Exactly (count, ptn) ->
+      match (count>0) with
+      | true ->
+        match (helper ptn cellList result) with
+        | Some (ptnCells, remCells) -> 
+          helper (Exactly (count-1, ptn)) remCells ptnCells
+        | _ -> None
+      | false -> Some (result, cellList)
+      
+    | FewerThan (count, ptn) ->
+      match count>0 with
+      | false -> None
+      | true ->
+        let nested = helper (ZeroOrMore ptn) cellList List.empty
+        match nested with
+        | Some (ptnCells, remCells) -> 
+          match (ptnCells.Length >= count) with
+          | false -> Some (result@ptnCells, remCells)
+          | true -> 
+            let subset = List.take (count-1) ptnCells
+            let rest = cellList.[subset.Length ..]
+            Some (result@subset, rest)
+        | _ -> failwith "FewerThan only returns 'None' when (count < 0)"
+    
+    | Either (a, b) ->
+      let aTuple = helper a cellList result 
+      let bTuple = helper b cellList result
+      match (aTuple, bTuple) with
+      | Some (aResult, _), Some (bResult, _) ->
+        match aResult.Length >= bResult.Length with
+        | true -> aTuple
+        | false -> bTuple
+      | Some x, None | None, Some x ->
+        Some x
+      | _ -> None
+
+    | Sequence ptnList ->
+      match ptnList with
+        | [] -> Some (result, cellList)
+        | h::t -> 
+          let option = helper h cellList result
+          match option with
+          | None -> option
+          | Some (ptnResult, rest) -> 
+            helper (Sequence t) rest ptnResult
+
+    | Anything ->
+      match cellList with
+      | [] -> None
+      | h::t -> 
+        Some (result@[h], t)
+
+    | EndOfCells ->
+      match cellList with
+      | _::_ -> None
+      | [] -> Some (result, cellList)
+  
+  match (helper _ptn _cells []) with
+  | Some (ptnResult, _) -> Some ptnResult
+  | _ -> None
+
+let find ptn cells = 
+  let rec finder (lst:Cell List) (i:int) =
+    let result = patternMatch ptn lst
+    match result with
+    | None ->
+      match lst.Length = 0 with
+      | true -> None
+      | false ->
+        finder lst.Tail (i + 1)
+    | Some x -> Some (x, offset) 
+  finder cells 0
+  
+let map (func:(Cell List->Cell List)) (ptn:Pattern) (cells:Cell List) = 
+  let rec helper (cList:Cell List) (rList:Cell List) =
+    match cList.IsEmpty with
+    | true -> rList
+    | false ->
+      match (find ptn cList) with
+      | None -> rList@cList
+      | Some (x, offset) ->
+        let unmatched, transformed = cList.[..(offset-1)], func x
+        let nResult = rList@unmatched@transformed
+        let startIndex = 
+          match offset + x.Length with
+          | 0 -> offset+1
+          | v -> v
+        helper cList.[startIndex..] nResult
+  helper cells List.empty
+
---
+++ File2.fs
@@ -0,0 +1,164 @@
+﻿module Patterns
+#endif
+
+type Cell =
+| Black
+| White 
+| Unknown
+
+type Pattern = 
+| BlackP | WhiteP | UnknownP
+|OneOrMore of Pattern
+|Exactly of int * Pattern | FewerThan of int * Pattern
+|Either of Pattern * Pattern
+|Anything | EndOfCells
+|Sequence of Pattern List
+|ZeroOrMore of Pattern
+
+
+let charToCell c =
+  match c with 
+  | 'b'| 'B' -> Black
+  | 'w'| 'W'-> White
+  | _ -> Unknown
+
+let cellToChar cell =
+  match cell with 
+  | Black -> "b"
+  | White -> "w"
+  | Unknown -> "." 
+
+let toCells (str:string) =
+  let rec perStr index cellList = 
+    match index < str.Length with
+    | true -> 
+            let cell = charToCell str.[index]
+            perStr (index+1) (cell::cellList)
+    | false -> cellList
+  List.rev (perStr 0 [])
+
+let fromCells (cells: Cell List) = 
+  let x = List.map(cellToChar) cells
+  List.fold (fun state e -> state + e) "" x
+
+let ptnToCell (ptn: Pattern) (cell:Cell) =
+    match ptn with
+    | BlackP -> Black = cell
+    | WhiteP -> White = cell
+    | UnknownP -> Unknown = cell
+    | Anything -> true
+    | _ -> failwith "Pattern not recognized"   
+
+let patternMatch (ptn:Pattern) (lst:Cell list) : Option<Cell List> =
+  let check (ptn:Pattern) (cells:Cell list) =
+    match cells with 
+    | [] -> None
+    | x::_ -> 
+      match ptnToCell ptn x with
+      | true -> Some x
+      | _ -> None
+
+  let rec Adder (p:Pattern) (cellList:Cell list) (rtn:Cell list) =
+    match p with
+    | BlackP | WhiteP | UnknownP  -> 
+        let ans = check ptn cellList
+        match ans with
+        | Some a -> Some (rtn @ (a::[]), cellList.Tail)
+        | _ -> None 
+        
+    |ZeroOrMore a -> 
+        match lst.Length > 0 with
+        | true ->
+            let ans = adder p cellList rtn
+            match ans with
+            | None -> Some (rtn, cellList)
+            | Some (ptnCells, rest) -> helper p rest ptnCells
+        | false -> Some (rtn, cellList)
+
+    | OneOrMore a ->
+      match cellList.Length > 0 with
+      | true ->
+        let ans = Adder (ZeroOrMore a) cellList rtn
+        match ans with
+        | Some (ptnCells, rest) -> 
+            match (ptnCells.Length > 0 ) with
+            | false -> None
+            | true -> Some (ptnCells, rest)
+        | _ -> None
+      | false -> None
+
+    | Exactly (count, a) ->
+      match (count > 0) with
+      | true ->
+        match (Adder a cellList rtn) with
+        | Some (ptnCells, rest) ->  Adder (Exactly (count-1, a)) rest ptnCells
+        | _ -> None
+      | false -> Some (rtn, cellList)
+
+    | FewerThan (count, a) ->
+      match count > 0 with
+      | true ->
+        let ans = Adder (ZeroOrMore a) cellList []
+        match ans with
+        | Some (ptnCells, rest) -> 
+          match (ptnCells.Length > count) with
+          | false -> Some (rtn @ ptnCells, rest)
+          | true -> 
+            let alist = List.take (count-1) ptnCells
+            let rem = cellList.[alst.Length ..]
+            Some (rtn @ alist, rem)
+        | _ -> failwith "FewerThan only returns 'None' when (count < 0)"
+      | false -> None
+
+    | Sequence ptns ->
+      match ptns with
+        | h::t -> 
+          let ans = Adder h cellList rtn
+          match ans with
+          | None -> ans
+          | Some (ptnResult, rest) -> helper (Sequence t) rest ptnResult
+        | [] -> Some (rtnt, cellList)
+
+    | Anything ->
+      match cellList with
+      | [] -> None
+      | h::t -> 
+        Some (rtn @ (h::[]), t)
+    
+    |EndOfCells -> match lst with |[] -> Some [] | _ -> None  
+  
+    match (Adder ptn lst []) with
+    | Some (x, _) -> Some x
+    | _ -> None
+
+let find ptn cells = 
+  let rec finder (lst:Cell List) (i:int) =
+    let result = patternMatch ptn lst
+    match result with
+    | None ->
+      match lst.Length = 0 with
+      | true -> None
+      | false ->
+        finder lst.Tail (i + 1)
+    | Some x -> Some (x, i) 
+  finder cells 0
+
+   
+let map (func:(Cell List->Cell List)) (ptn:Pattern) (cells:Cell List) = 
+  let rec mapper (lst:Cell List) (alist:Cell List) =
+    match lst.IsEmpty with
+    | true -> alist
+    | false ->
+      match (find ptn lst) with
+      | None -> alist @ lst
+      | Some (x, i) ->
+        let (y, trans) = (alist.[.. (i - 1)], func x
+        let Result = alist @ y @ trans
+        let startIndex = 
+          match i + x.Length with
+              | 0 -> i + 1
+              | v -> v
+        mapper lst.[startIndex..] Result
+     mapper cells []
+
+
